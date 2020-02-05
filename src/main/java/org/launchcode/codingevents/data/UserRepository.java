50792147c88fdd4b7745ea6b1c8ed38979a93557
Elisha Johnson <elishamagicjohnson@gmail.com>
2020-02-04 18:05:46 -0600

    chapter 19

--- src/main/java/org/launchcode/codingevents/data/UserRepository.java
+++ src/main/java/org/launchcode/codingevents/data/UserRepository.java
@@ -3,6 +3,7 @@ package org.launchcode.codingevents.data;
 import org.launchcode.codingevents.models.User;
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     User findByUsername(String username);
